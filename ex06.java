4d81d5e67e1e5cd43f23300a81a585bb95c3caa8
Cristhian <rodriguescristhian03@gmail.com>
2022-08-28 06:00:21 -0300

    estudando

---
+++ ex06.java
@@ -0,0 +1,25 @@
+/*Desenvolva o algoritmo de um programa para calcular a média de duas notas das avaliações de um aluno.*/
+
+import java.util.Scanner;
+
+public class ex06 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Informe a nota da prova 01: ");
+        float nota1 = in.nextFloat();
+
+        System.out.print("Informe a nota da prova 02: ");
+        float nota2 = in.nextFloat();
+        
+        float med = (nota1 + nota2)/2;
+
+        System.out.println("A media do aluno ficou em "+ med + " pontos.");
+
+        
+        
+
+        
+    }
+    
+}
---
+++ ex07.java
@@ -0,0 +1,24 @@
+/* Construa um algoritmo que dadas as entradas: distância do trajeto e velocidade média da viagem, informe o tempo que uma família levará saindo de sua cidade de férias até o destino previsto. Após o cálculo, o algoritmo deve mostrar o tempo calculado. */
+
+import java.util.Scanner;
+
+public class ex07 {
+    public static void main(String[] args) {
+
+        float dist , vel , temp;
+        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Quantos km de viagem para seu destino de férias? ");
+
+        dist = in.nextFloat();
+
+        System.out.print("Qual velocidade constante até o destino final? ");
+
+        vel =  in.nextFloat();
+
+        temp = dist/vel;
+
+        System.out.println("o tempo até o destino é de aproximadamente "+ temp + " hrs");
+    }
+    
+}
